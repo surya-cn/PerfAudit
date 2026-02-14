82d816cbad909ed444040b44d22b776b89e0c468
SURYA C N <47944058+surya-cn@users.noreply.github.com>
2026-02-14 08:19:39 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace EA_Performance_Audit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBoxProcesses = new System.Windows.Forms.ComboBox();
+            this.btnToggle = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblLiveUsage = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxProcesses
+            // 
+            this.comboBoxProcesses.FormattingEnabled = true;
+            this.comboBoxProcesses.Location = new System.Drawing.Point(319, 110);
+            this.comboBoxProcesses.Name = "comboBoxProcesses";
+            this.comboBoxProcesses.Size = new System.Drawing.Size(331, 24);
+            this.comboBoxProcesses.TabIndex = 0;
+            // 
+            // btnToggle
+            // 
+            this.btnToggle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToggle.ForeColor = System.Drawing.Color.Orange;
+            this.btnToggle.Location = new System.Drawing.Point(97, 163);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(152, 51);
+            this.btnToggle.TabIndex = 1;
+            this.btnToggle.Text = "Start";
+            this.btnToggle.UseVisualStyleBackColor = true;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.ForeColor = System.Drawing.Color.Orange;
+            this.btnRefresh.Location = new System.Drawing.Point(97, 254);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(152, 49);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatus.ForeColor = System.Drawing.Color.LightGreen;
+            this.lblStatus.Location = new System.Drawing.Point(94, 387);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 16);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblLiveUsage
+            // 
+            this.lblLiveUsage.AutoSize = true;
+            this.lblLiveUsage.BackColor = System.Drawing.Color.Transparent;
+            this.lblLiveUsage.ForeColor = System.Drawing.Color.LightGreen;
+            this.lblLiveUsage.Location = new System.Drawing.Point(656, 387);
+            this.lblLiveUsage.Name = "lblLiveUsage";
+            this.lblLiveUsage.Size = new System.Drawing.Size(48, 16);
+            this.lblLiveUsage.TabIndex = 4;
+            this.lblLiveUsage.Text = "Usage";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Turquoise;
+            this.label1.Location = new System.Drawing.Point(375, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 36);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select Process";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblLiveUsage);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnToggle);
+            this.Controls.Add(this.comboBoxProcesses);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PerfAudit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxProcesses;
+        private System.Windows.Forms.Button btnToggle;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblLiveUsage;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace EA_Performance_Audit
+{
+    public partial class Form1 : Form
+    {
+        private Process _targetProcess;
+        private TimeSpan _lastTotalProcessorTime;
+        private DateTime _lastSampleTime;
+        private List<string> _logEntries = new List<string>();
+        private bool _isExported = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "PerfAudit v1.0";
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; // Disables resizing
+            this.MaximizeBox = false; // Removes the maximize button
+            this.StartPosition = FormStartPosition.CenterScreen; this.TopMost = true; // Keeps the tool on top of your game/test window
+            RefreshProcessList();
+            this.FormClosing += (s, e) => SafeExport("Manual App Closure");
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e) => RefreshProcessList();
+
+        private void RefreshProcessList()
+        {
+            comboBoxProcesses.Items.Clear();
+            var procs = Process.GetProcesses().OrderBy(p => p.ProcessName);
+            foreach (var p in procs) comboBoxProcesses.Items.Add($"{p.ProcessName} (PID: {p.Id})");
+        }
+
+        private void btnToggle_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) SafeExport("Manual Stop");
+            else StartMonitoring();
+        }
+
+        private void StartMonitoring()
+        {
+            try
+            {
+                string selected = comboBoxProcesses.SelectedItem.ToString();
+                int pid = int.Parse(selected.Split(new[] { "PID: " }, StringSplitOptions.None)[1].Replace(")", ""));
+
+                _targetProcess = Process.GetProcessById(pid);
+                _targetProcess.EnableRaisingEvents = true;
+                _targetProcess.Exited += (s, e) => SafeExport("Target Process Terminated");
+
+                _lastTotalProcessorTime = _targetProcess.TotalProcessorTime;
+                _lastSampleTime = DateTime.Now;
+                _logEntries.Clear();
+                _isExported = false;
+
+                timer1.Start();
+                lblStatus.Text = "Status: RECORDING...";
+                lblStatus.ForeColor = System.Drawing.Color.LightGreen; // Set to Green immediately
+                lblLiveUsage.ForeColor = System.Drawing.Color.LightGreen;
+                btnToggle.Text = "Stop & Export";
+                btnToggle.BackColor = System.Drawing.Color.LightCoral;
+            }
+            catch { MessageBox.Show("Please select a process first."); }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_targetProcess == null || _targetProcess.HasExited) return;
+
+            var currentTotalTime = _targetProcess.TotalProcessorTime;
+            var currentSampleTime = DateTime.Now;
+
+            double cpuUsedMs = (currentTotalTime - _lastTotalProcessorTime).TotalMilliseconds;
+            double totalMsPassed = (currentSampleTime - _lastSampleTime).TotalMilliseconds;
+            double cpuUsage = (cpuUsedMs / (Environment.ProcessorCount * totalMsPassed)) * 100;
+
+            _lastTotalProcessorTime = currentTotalTime;
+            _lastSampleTime = currentSampleTime;
+
+            string line = $"{currentSampleTime:yyyy-MM-dd},{currentSampleTime:HH:mm:ss},{_targetProcess.ProcessName},{Math.Round(cpuUsage, 2)}";
+            _logEntries.Add(line);
+
+            // Live Update and Visual Warning
+            lblLiveUsage.Text = $"CPU: {Math.Round(cpuUsage, 2)}%";
+            if (cpuUsage > 15.0)
+            {
+                lblLiveUsage.ForeColor = System.Drawing.Color.Tomato;
+                lblStatus.Text = "Status: WARNING - HIGH USAGE";
+            }
+            else
+            {
+                lblLiveUsage.ForeColor = System.Drawing.Color.LightGreen;
+                lblStatus.Text = "Status: RECORDING...";
+            }
+        }
+
+        private void SafeExport(string reason)
+        {
+            lock (this)
+            {
+                if (_isExported || _logEntries.Count == 0) return;
+                _isExported = true;
+                timer1.Stop();
+
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), $"EA_Audit_{DateTime.Now:yyyyMMdd_HHmmss}.csv");
+                File.WriteAllLines(path, new[] { "Date,Time,Process,CPU%" }.Concat(_logEntries));
+
+                if (this.InvokeRequired) this.BeginInvoke((Action)(() => ResetUI(reason)));
+                else ResetUI(reason);
+            }
+        }
+
+        private void ResetUI(string msg)
+        {
+            lblStatus.Text = $"Exported: {msg}";
+            lblStatus.ForeColor = System.Drawing.Color.White; // Reset color
+            lblLiveUsage.Text = "CPU: 0.00%";
+            lblLiveUsage.ForeColor = System.Drawing.Color.White;
+            btnToggle.Text = "Start Monitoring";
+            btnToggle.BackColor = System.Drawing.Color.PaleGreen;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
