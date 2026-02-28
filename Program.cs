e3b828aa2886a9a6709a89e192a7717395a8f027
SURYA C N <47944058+surya-cn@users.noreply.github.com>
2026-03-01 00:03:15 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,9 +8,7 @@ namespace EA_Performance_Audit
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
