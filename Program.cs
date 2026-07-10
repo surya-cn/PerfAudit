01b8cdc345d871c282b1a3afda0f693a41db5c6b
SURYA C N <47944058+surya-cn@users.noreply.github.com>
2026-07-10 12:18:19 +0530

    Version 1.8 update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +15,27 @@ namespace PerfAudit
         [STAThread]
         static void Main()
         {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                try
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        FileName = Application.ExecutablePath,
+                        UseShellExecute = true,
+                        Verb = "runas"
+                    };
+                    Process.Start(startInfo);
+                }
+                catch (Win32Exception)
+                {
+                    // User canceled the UAC prompt
+                }
+                return; // Exit the current non-elevated instance
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
